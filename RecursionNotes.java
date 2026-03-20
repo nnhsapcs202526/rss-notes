8d1259ebaa63be848c33ea4d0751d15a10292e17
mrcallaghan <mcallaghan@naperville203.org>
2026-03-20 11:52:22 -0500

    Update RecursionNotes.java

--- RecursionNotes.java
+++ RecursionNotes.java
@@ -19,7 +19,7 @@ public class RecursionNotes
 
         return strReversed;
     }
-    
+
     public static String reverse(String str)
     {
         //  must have a terminating case, or base case (i.e., "the empty loaf")
@@ -27,52 +27,51 @@ public class RecursionNotes
         {
             return str;
         }
-        
+
         // take a small step towards the solution (i.e., "one slice of bread")
         String firstChar = str.substring(0, 1);
         String restOfString = str.substring(1);
-        
+
         // recurse - call this method with a simpler problem (i.e., "dreams within dreams")
         String restOfStringReversed = reverse(restOfString);
 
-        
         String strReversed = restOfStringReversed + firstChar;
         return strReversed;
-            
+
     }
-    
+
     public static ArrayList<Integer> makeList( int n )
     {
         ArrayList<Integer> tempList = null;
-        if( n <= 0 )  					// The smallest list we can make (terminating condition)
+        if( n <= 0 )                      // The smallest list we can make (terminating condition)
         {
             tempList = new ArrayList<Integer>();
             return tempList;
         }
-        else        					// All other lists are created here
+        else                            // All other lists are created here
         {
-            tempList = makeList( n - 1 );  	        // Pass on the smaller problem to solve elsewhere
-            tempList.add( n );             	        //Add our contribution to solving the larger problem 
+            tempList = makeList( n - 1 );              // Pass on the smaller problem to solve elsewhere
+            tempList.add( n );                         //Add our contribution to solving the larger problem 
         }
 
-        return tempList;	
+        return tempList;    
     }
-    
+
     public static ArrayList<Integer> reverseList( ArrayList<Integer> tList )
-{
-	ArrayList<Integer> list = ListMethods.deepClone( tList );
-	if(( list.size() == 1 ) || ( list.size() == 0 ))
-	{
-		return list;
-	}
-	else
-	{
-		Integer tempInt = list.remove( 0 );
-		list = reverseList( list );
-		list.add( tempInt );
-	}
-	
-	return list;
-  }
+    {
+        ArrayList<Integer> list = ListMethods.deepClone( tList );
+        if(( list.size() == 1 ) || ( list.size() == 0 ))
+        {
+            return list;
+        }
+        else
+        {
+            Integer tempInt = list.remove( 0 );
+            list = reverseList( list );
+            list.add( tempInt );
+        }
+
+        return list;
+    }
 
 }
